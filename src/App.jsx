c6f5122c7e83d937cb77be2adcd6163f971bba36
JerryShum <jerryshum2636@gmail.com>
2024-05-18 11:55:15 -0400

    feat: added phone dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <div className="flex justify-center items-center rounded-xl border-2 border-black w-2/5 h-[99vh] mx-auto">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
